e9520ba8f2dbb4b55a632ba17f33018b3b0b27d3
gianluca.dacosta <gianluca.dacosta@nubilaria.com>
2018-11-29 15:33:34 +0100

    feat(article): Add ListArticle use case

--- app/Http/Controllers/ListArticlesController.php
+++ app/Http/Controllers/ListArticlesController.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use LaravelDay\Article\UseCase\ListArticles\ListArticles;
+
 class ListArticlesController extends Controller
 {
     public function __invoke()
     {
-        return response()->json([]);
+        $listArticles = new ListArticles();
+        $articles = $listArticles();
+
+        return response()->json($articles);
+        /*
+        return response()->json([
+            [
+                'title' => 'Articolo 1',
+                'body' => 'Questo è un articolo',
+                'creationDate' => '2018-11-29 00:00:00'
+            ]
+        ]);
+        */
     }
 }
